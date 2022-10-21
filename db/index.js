aaea60729108a3fad8c0e138ad1301e1f59c1927
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-21 18:53:39 -0400

    edited index.js in db, server.js, and created routes directory

--- db/index.js
+++ db/index.js
@@ -3,12 +3,17 @@ const mongoose = require('mongoose')
 let MONGODB_URI =
   'mongodb+srv://sramalho:22emperoR@subwaysurferprojectclus.xs2jkjp.mongodb.net/subwaySurfersDatabase'
 
-mongoose.connect(MONGODB_URI)
-then(() => {
-  console.log('Riding the lines with MongoDB 🚇')
-}).catch((e) => {
-  console.error('Connection error', e.message)
-})
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('Riding the lines with MongoDB 🚇')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
 
 //remove this line after final product is complete
 mongoose.set('debug', true)
+const db = mongoose.connection
+
+module.exports = db
--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 const express = require('express')
+// const routes = require('./routes')
+const db = require('./db')
 const cors = require('cors')
 const logger = require('morgan')
 
 const app = express()
+
 const PORT = process.env.PORT || 3001
 
-// 3rd party middleware
 app.use(cors())
 app.use(logger('dev'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//routes
-app.get('/', (req, res) => {
-  res.send({ subwaySurfed: true })
-})
-
-app.get(
-  '/middleware',
-  (req, res, next) => {
-    console.log('this is middleware')
-    next()
-  },
-  (req, res) => {
-    console.log('FROM LINE 28: this is the function of the response completing')
-    res.send('response completed')
-  }
-)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.listen(PORT, () => {
   console.log(`Subway 🚇 server listening on port: ${PORT}`)
