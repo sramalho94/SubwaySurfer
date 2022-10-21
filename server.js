302f9d22e0ce6a5fdbb59b8f4efb01c1f7610d28
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-20 20:37:18 -0400

    added test get request to server.js and it worked

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send('Subway surfers go!')
+})
+
 app.listen(PORT, () => {
   console.log(`Subway 🚇 server listening on port: ${PORT}`)
 })
