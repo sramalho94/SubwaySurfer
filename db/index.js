5dacb27654100504f902a989137b854668768b1d
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-21 16:20:37 -0400

    establshed connection to mongo atlas db

--- db/index.js
+++ db/index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/subwaySurferDatabase'
+let MONGODB_URI =
+  'mongodb+srv://sramalho:22emperoR@subwaysurferprojectclus.xs2jkjp.mongodb.net/subwaySurfersDatabase'
 
 mongoose.connect(MONGODB_URI)
 then(() => {
   console.log('Riding the lines with MongoDB 🚇')
+}).catch((e) => {
+  console.error('Connection error', e.message)
 })
+
+//remove this line after final product is complete
+mongoose.set('debug', true)
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(logger('dev'))
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 
 //routes
 app.get('/', (req, res) => {
