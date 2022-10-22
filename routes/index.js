b652dcfb8c090c3b91714aa929c18d7b040adc46
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-22 14:19:01 -0400

    added route for createUser controller

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const controllers = require('../controllers')
 const { db } = require('../db')
 
 router.get('/', (req, res) => res.send('This is root!'))
+
 router.get('/users', controllers.getAllUsers)
+
+router.post('/users', controllers.createUser)
 
 module.exports = router
