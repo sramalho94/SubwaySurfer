c05b2ec48a2f4b3b7b4e07eb9b1caec279722eab
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-23 17:07:42 -0400

    rendered reviews on front end through app.js as test for axios connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  return <div className="App"></div>
+  const [reviews, updateReviews] = useState([])
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/reviews')
+      updateReviews(response.data.reviews)
+    }
+    apiCall()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>All Reviews Here</h1>
+      {reviews.map((review) => (
+        <div key={review._id}>
+          {/* <h1>{review.line_id.line}</h1> */}
+          <h2>{review.review}</h2>
+          <h2>{review.rating}</h2>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
