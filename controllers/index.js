3d94e45f9c9ac116d0aa308dbabe7c2e1936e398
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-23 19:18:21 -0400

    made updateUser controller

--- controllers/index.js
+++ controllers/index.js
@@ -36,6 +36,17 @@ const getUserById = async (req, res) => {
   }
 }
 
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    res.status(200).json(plant)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 const createSubway = async (req, res) => {
   try {
     const subway = await new Subway(req.body)
@@ -72,6 +83,7 @@ module.exports = {
   getAllUsers,
   createUser,
   getUserById,
+  updateUser,
   getAllReviews,
   createReview,
   createSubway
