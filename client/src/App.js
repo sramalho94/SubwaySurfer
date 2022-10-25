751d2482ab2d9c1b076efc342059d11bf78beb5f
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-25 14:14:55 -0400

    added Nav component into App component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Reviews from './components/Reviews'
 import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Reviews />} />
-      </Routes>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Reviews />} />
+        </Routes>
+      </main>
     </div>
   )
 }
