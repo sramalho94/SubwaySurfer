ab24b3ee2b672805350f7c21a1f8f8e63edd3720
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-27 11:32:04 -0400

    more styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,7 +4,7 @@ const Nav = (props) => {
   console.log(props.isLoggedIn)
   return(
     <nav className ="navbar">
-      <div className="navbar">
+      <div className ="navcontainer">
         <Link to="/" className='navlink'>Reviews</Link>
         <Link to="/write-review" className='navlink'>Write Review</Link>
         {props.isLoggedIn ? (<div>Hello {props.firstName}, You are logged in</div>) 
