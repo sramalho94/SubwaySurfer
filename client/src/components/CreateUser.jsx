6e97a0de051bc7a66ae12378fe5299228db0c1e6
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-16 09:55:19 -0400

    removed first and last name input in form and set values to default in state

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -2,44 +2,55 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 
-const CreateUser = (props)=>{
-
-  const [formState, setFormState]= useState({ userName: '', firstName: '', lastName: '', password:''})
+const CreateUser = (props) => {
+  const [formState, setFormState] = useState({
+    userName: '',
+    firstName: 'default',
+    lastName: 'default',
+    password: ''
+  })
   const navigate = useNavigate()
-  const handleChange = (event)=>{
-    setFormState({...formState, [event.target.id]: event.target.value})
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
   }
-  const handleSubmit = async (event)=>{
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    let createdUser = await axios.post('/api/users', 
-      formState)
-      .then((response)=>{
+    let createdUser = await axios
+      .post('/api/users', formState)
+      .then((response) => {
         props.setLogIn(true)
         props.setFirstName(formState.firstName)
-      navigate('/')
+        navigate('/')
       })
-      .catch((error)=>{
+      .catch((error) => {
         console.log(error)
-      });
-      
+      })
   }
-  return(
-    
+  return (
     <div className="CreateUserForm">
       <marquee>
-        <img className='Train-car'src='https://cdn.pixabay.com/photo/2017/11/13/08/08/silver-2944881_960_720.png'></img>
+        <img
+          className="Train-car"
+          src="https://cdn.pixabay.com/photo/2017/11/13/08/08/silver-2944881_960_720.png"
+        ></img>
       </marquee>
       <h3>To Sign Up, Fill Out:</h3>
       <form onSubmit={handleSubmit} className="form">
-        <label htmlFor='userName'>UserName:</label>
-        <input id='userName' value={formState.userName} onChange={handleChange}/>
-        <label htmlFor='firstName'>FirstName:</label>
-        <input id='firstName' value={formState.firstName} onChange={handleChange}/>
-        <label htmlFor='lastName'>LastName:</label>
-        <input id='lastName' value={formState.lastName} onChange={handleChange}/> 
-        <label htmlFor='password'>Password:</label>
-        <input id='password'value={formState.password} onChange={handleChange}/>       
-        <button type='submit' className='button'>Submit</button>
+        <label htmlFor="userName">UserName:</label>
+        <input
+          id="userName"
+          value={formState.userName}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          value={formState.password}
+          onChange={handleChange}
+        />
+        <button type="submit" className="button">
+          Submit
+        </button>
       </form>
     </div>
   )
