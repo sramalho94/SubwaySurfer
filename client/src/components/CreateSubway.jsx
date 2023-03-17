84676e56f152ce6ba4f43acf9034bb1b920f2296
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-17 12:02:16 -0400

    replaced marquee tag

--- client/src/components/CreateSubway.jsx
+++ client/src/components/CreateSubway.jsx
@@ -1,42 +1,47 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router"
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 const CreateSubway = () => {
-const navigate = useNavigate()
-const [formState, setFormState]= useState({line:'', description:'', logo:''})
-const handleChange = (event) => {
-  setFormState({...formState, [event.target.id]: event.target.value})
-}
-const handleSubmit = async (event) => {
-  event.preventDefault()
-  console.log(formState)
-  let newSubway = await axios.post('/api/subways', 
-  formState)
-  .then((response)=> {
-    console.log(response)
-    setFormState({line:'', description:'', logo:''})
-    navigate('/')
+  const navigate = useNavigate()
+  const [formState, setFormState] = useState({
+    line: '',
+    description: '',
+    logo: ''
   })
-  .catch((error)=> {
-    console.log(error)
-  });
-  console.log(newSubway)
-}
-return (
-  <div className="CreateUserForm">
-    <h3>Add Another Line here:</h3>
-    <form className="form" onSubmit={handleSubmit}>
-      <label htmlFor="line">Line:</label>
-      <input id='line' value={ formState.line} onChange={ handleChange}></input>
-      <label htmlFor="description">Description:</label>
-      <textarea id='description' value={ formState.description} onChange={ handleChange}></textarea>
-      <label htmlFor="logo">Logo Image Url:</label>
-      <input id='logo' value={ formState.logo} onChange={ handleChange}></input>
-      <button type='submit'>Add Line</button>
-    </form>
-  </div>
-)
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
 
+    let newSubway = await axios
+      .post('/api/subways', formState)
+      .then((response) => {
+        setFormState({ line: '', description: '', logo: '' })
+        navigate('/')
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  return (
+    <div className="CreateUserForm">
+      <h3>Add Another Line here:</h3>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="line">Line:</label>
+        <input id="line" value={formState.line} onChange={handleChange}></input>
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          value={formState.description}
+          onChange={handleChange}
+        ></textarea>
+        <label htmlFor="logo">Logo Image Url:</label>
+        <input id="logo" value={formState.logo} onChange={handleChange}></input>
+        <button type="submit">Add Line</button>
+      </form>
+    </div>
+  )
 }
 export default CreateSubway
