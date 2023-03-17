7c837a3fbb8a8249efd981004c9faaed412753e2
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-17 12:03:26 -0400

    replaced marquee tag

--- client/src/components/SubwayLines.jsx
+++ client/src/components/SubwayLines.jsx
@@ -1,35 +1,29 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-const SubwayLines = () =>{
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+const SubwayLines = () => {
   const [subways, updateSubways] = useState([])
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     const apiCall = async () => {
-    let response = await axios.get('/api/subways')
-    console.log(response.data.subways)
-    updateSubways(response.data.subways)
+      let response = await axios.get('/api/subways')
+      updateSubways(response.data.subways)
     }
     apiCall()
-    console.log(subways)
-  },[])
+  }, [])
 
-  return(
+  return (
     <div>
       <h1 className="title">All Subway lines here:</h1>
-    <div className="Reviews">
-      {subways.map((subway)=>(
-        <div key={subway._id} className='review'>
-          <img src={subway.logo} className='logo'></img>
-          <h3>{subway.description}</h3>
-
-        </div>
-      )
-
-      )}
-    </div>
+      <div className="Reviews">
+        {subways.map((subway) => (
+          <div key={subway._id} className="review">
+            <img src={subway.logo} className="logo" alt={subway._id}></img>
+            <h3>{subway.description}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default SubwayLines
-
