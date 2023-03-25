d16155699aabd97dc35ad2fcc02bb8646f7433f0
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-25 13:28:42 -0400

    new styling for CrateSubway component

--- client/src/components/CreateSubway.jsx
+++ client/src/components/CreateSubway.jsx
@@ -27,21 +27,47 @@ const CreateSubway = () => {
       })
   }
   return (
-    <div className="CreateUserForm">
+    <div className="bg-gray-200 min-h-screen pt-5">
+      <h1 className="font-bold text-4xl"> Don't see your line or bus?</h1>
+      <p className="pt-6">
+        {' '}
+        New York City is constantly evolving. If you don't see your train or bus
+        listed on our site you can add it by filling out this form.
+      </p>
       <h3>Add Another Line here:</h3>
-      <form className="form" onSubmit={handleSubmit}>
-        <label htmlFor="line">Line:</label>
-        <input id="line" value={formState.line} onChange={handleChange}></input>
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          value={formState.description}
-          onChange={handleChange}
-        ></textarea>
-        <label htmlFor="logo">Logo Image Url:</label>
-        <input id="logo" value={formState.logo} onChange={handleChange}></input>
-        <button type="submit">Add Line</button>
-      </form>
+      <div className="flex flex-row justify-center">
+        <form
+          className="flex flex-col  bg-white w-2/3 h-72 p-2 border-b-4 border-blue-500 shadow-xl  text-[#0f61a9]  font-bold text-lg mt-20"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-row justify-between pb-1">
+            <label htmlFor="line">Line:</label>
+            <input
+              id="line"
+              value={formState.line}
+              onChange={handleChange}
+              className="bg-blue-100 rounded-xl"
+            ></input>
+          </div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            value={formState.description}
+            onChange={handleChange}
+            className="bg-blue-100 rounded-xl pb-3 mb-1"
+          ></textarea>
+          <div className="flex flex-row justify-between mb-8">
+            <label htmlFor="logo">Logo Image Url:</label>
+            <input
+              id="logo"
+              value={formState.logo}
+              onChange={handleChange}
+              className="bg-blue-100 rounded-xl"
+            ></input>
+          </div>
+          <button type="submit">Add Line</button>
+        </form>
+      </div>
     </div>
   )
 }
