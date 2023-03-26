7c8e9c3199578f5d29829c4f9f2a7809f7e670da
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-26 14:29:19 -0400

    small tweaks

---
+++ client/src/components/TrainCar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TrainCar = () => {
+  return (
+    <div className="train-car-container min-w-max">
+      <img
+        className="Train-car"
+        src="https://cdn.pixabay.com/photo/2017/11/13/08/08/silver-2944881_960_720.png"
+        alt="traincar"
+      ></img>
+    </div>
+  )
+}
+
+export default TrainCar
