e11b9893b5c39c0c1eeaf11e853a7c42c2b0c10a
Stephan D Ramalho <sramalho@fordham.edu>
2023-03-27 17:09:00 -0400

    styling for editReview component

--- client/src/components/EditReview.jsx
+++ client/src/components/EditReview.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router'
+import TrainCar from './TrainCar'
+import { BASE_URL } from '../globals'
 
 const EditReview = (props) => {
   const navigate = useNavigate()
@@ -13,7 +15,7 @@ const EditReview = (props) => {
   useEffect(() => {
     const getReviewId = async () => {
       try {
-        let response = await axios.get(`/api/reviews/${id}`)
+        let response = await axios.get(`${BASE_URL}/api/reviews/${id}`)
         setFormState(response.data.review)
       } catch (error) {
         console.log(error)
@@ -23,38 +25,57 @@ const EditReview = (props) => {
   }, [id])
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await axios.put(`/api/reviews/${id}`, formState)
+    await axios.put(`${BASE_URL}/api/reviews/${id}`, formState)
     navigate('/')
   }
 
   return (
-    <div className="CreateUserForm">
-      <div className="train-car-container">
-        <img
-          className="Train-car"
-          src="https://cdn.pixabay.com/photo/2017/11/13/08/08/silver-2944881_960_720.png"
-          alt="traincar"
-        ></img>
+    <div className="bg-gray-200 min-h-screen  pb-11">
+      <div
+        style={{
+          backgroundImage: `url('/subwaybg2.jpg')`,
+          backgroundPosition: 'top'
+        }}
+        className="h-96 p-1 flex flex-col justify-center mb-5"
+      >
+        <TrainCar />
       </div>
-      <h1>Edit Review Below</h1>
+      <h1 className="font-bold text-4xl px-1">
+        {' '}
+        Want to edit the review content?
+      </h1>
+      <p className="pt-6 px-1">
+        {' '}
+        Replace the form values to edit the content of this review.
+      </p>
+      <h1 className="text-xl px-2 font-bold py-2">Edit Review Below</h1>
       {formState && (
-        <form onSubmit={handleSubmit} className="form">
-          <label htmlFor="review">Review:</label>
-          <input
-            id="review"
-            defaultValue={formState.review}
-            onChange={handleChange}
-          />
-          <label htmlFor="rating">Rating:</label>
-          <input
-            id="rating"
-            defaultValue={formState.rating}
-            onChange={handleChange}
-          />
-          <button type="submit" className="button">
-            Submit Revision
-          </button>
-        </form>
+        <div className="flex flex-row justify-center">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col  bg-white md:w-2/3 md:h-72 p-2 border-b-4 border-blue-500 shadow-xl  text-[#0f61a9]  font-bold text-lg mt-20 justify-center "
+          >
+            <label htmlFor="review">Review:</label>
+            <textarea
+              id="review"
+              defaultValue={formState.review}
+              onChange={handleChange}
+              className="bg-blue-100 rounded-xl pb-3 mb-1"
+            />
+            <div className="flex flex-row justify-between mb-8">
+              <label htmlFor="rating">Rating:</label>
+              <input
+                id="rating"
+                defaultValue={formState.rating}
+                onChange={handleChange}
+                className="bg-blue-100 rounded-xl"
+              />
+            </div>
+            <button type="submit" className="button">
+              Submit Revision
+            </button>
+          </form>
+        </div>
       )}
     </div>
   )
